464fc9bb392ae5979bf0d58027d527e859cb7a01
Sebastien Coste <scoste@infopro.fr>
2017-11-10 14:08:57 +0100

    (WIP) adding contest on word

--- lambda/index.js
+++ lambda/index.js
@@ -4,6 +4,7 @@
 const Alexa = require('alexa-sdk');
 // Helpers
 const convertArrayToReadableString = require('./helpers/convertArrayToReadableString');
+const getAWord = require('./helpers/words');
 
 const appId = "amzn1.ask.skill.11532793-2767-420b-ae38-e2ae5e5a397a";  // TODO replace with your app ID (OPTIONAL).
 
@@ -38,12 +39,42 @@ const handlers = {
       // Save Name in Session Attributes
       if (userName) {
         this.attributes['userName'] = userName;
-        this.emit(':ask', `Ok ${userName} ! Let\'s get started.`, 'say ... "start" ... to start a contest');
+
+        this.emit(':ask', `Ok ${userName} ! Let\'s get started.`, 'say <break time="1s"/> "start" <break time="1s"/>  to start a contest');
       } else {
         this.emit(':ask', 'Sorry, I didn\'t recognise that name!', 'Tell me your name by saying: My name is, and then your name.');
       }
     },
+    'StartIntent': function () {
+        var word = getAWord();
+        this.attributes['userName'] = word;
 
+        this.emit(':ask',
+            `we\'re looking for a word of ${number} letters <break time="2s"/>  <say-as interpret-as="spell-out">${word}</say-as>` ,
+            'I repeat <break time="1s"/>  <say-as interpret-as="spell-out">${word}</say-as>');
+    },
+    'AnswerIntent': function () {
+
+        var userName = this.attributes['userName'];
+        if (!userName){
+            userName = "";
+        }
+        var word = this.attributes['word'];
+        var answer = this.attributes['answer'];
+        if (!word){
+            this.emit(':ask', ${userName} + ' start a contest by sating <break time="1s"/>  start', 'say <break time="1s"/>  start <break time="1s"/>  to start a contest.');
+        } else if (!answer){
+            this.emit(':ask', `I couldn\'t catch your answer ${userName} <break time="1s"/>  please repeat it`, 'please repeat your answer.');
+        } else {
+            if (word.trim().toLowerCase() === answer.trim().toLowerCase()){
+                this.emit(':ask', `Congratulations ${userName} <break time="1s"/>  you found the word ${word}. say <break time="1s"/>  start <break time="1s"/>  to start another contest.`,
+                    'say <break time="1s"/>  start <break time="1s"/>  to start another contest.');
+            } else {
+                this.emit(':ask', `Sorry ${userName} <break time="1s"/>  you said ${answer} but the answer was ${word}. say <break time="1s"/>  start <break time="1s"/>  to start another contest.`,
+                    'say <break time="1s"/>  start <break time="1s"/>  to start another contest.');
+            }
+        }
+    },
     'AMAZON.HelpIntent': function () {
         this.emit(':ask', 'speechOutput', 'reprompt');
     },
