b7263ab262d53ecd3ce63fab2ba62cc28e1c34e4
Sebastien Coste <teddy.coste@gmail.com>
2017-11-11 17:08:36 +0100

    upgrade deployment Process

--- publishEventProcess.sh
+++ publishEventProcess.sh
@@ -3,5 +3,5 @@ cd lambda
 npm install
 zip -r ../lambda.zip *
 cd ..
-aws lambda update-function-configuration --function-name AlexaSPellingEventProcess --description "Process events raised by alexa's skill Spelling Contest" --role arn:aws:iam::398750413546:role/LambdaFullAccess --handler indexSkill --timeout 10 --memory-size 512 --environment Variables="{ALEXA_APP_ID=amzn1.ask.skill.11532793-2767-420b-ae38-e2ae5e5a397a}" --profile alexa
+aws lambda update-function-configuration --function-name AlexaSPellingEventProcess --description "Process events raised by alexa's skill Spelling Contest" --role arn:aws:iam::398750413546:role/LambdaFullAccess --handler indexEventProcess.handler --timeout 10 --memory-size 512 --environment Variables="{ALEXA_APP_ID=amzn1.ask.skill.11532793-2767-420b-ae38-e2ae5e5a397a}" --profile alexa
 aws lambda update-function-code --function-name AlexaSPellingEventProcess --zip-file fileb://lambda.zip --profile alexa
