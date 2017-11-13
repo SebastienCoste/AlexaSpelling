0f4ead8a6db166873ca1ed48734b11b7f44eb03f
Sebastien Coste <SebastienCoste@users.noreply.github.com>
2017-11-13 09:05:49 +0100

    Number contest (#2)
    * integration of numbers contest
    
    * change branch Name
    
    * bug fix when Alexa selects the wrong intent

--- lambda/helpers/interaction.js
+++ lambda/helpers/interaction.js
@@ -27,10 +27,10 @@ module.exports = (function() {
       let userName = session.attributes['userName'];
       if (userName) {
         session.handler.state = stateContext.states.BETWEEN_QUESTIONS;
-        session.emitWithState('LaunchRequest')
+        return session.emitWithState('LaunchRequest')
       } else {
         // Welcome User for the First Time
-        session.emit(':ask', 'Welcome to spelling contest! Say ... "my name is" ... to bind your experience to you', 'Say "my name is" to bind your experience to you');
+        return session.emit(':ask', 'Welcome to spelling contest! Say ... "my name is" ... to bind your experience to you', 'Say "my name is" to bind your experience to you');
       }
     },
 
@@ -61,14 +61,12 @@ module.exports = (function() {
     },
 
     startContest: (session) => {
-      let word = getAWord();
-      session.attributes['word'] = word;
-      let number = word.trim().length;
-      session.handler.state = stateContext.states.QUESTIONNING;
-
-      session.emit(':ask',
-        `we\'re looking for a word of ${number} letters <break time="1s"/>  <say-as interpret-as="spell-out">${word}</say-as>`,
-        `I repeat <break time="0.5s"/>  <say-as interpret-as="spell-out">${word}</say-as>`);
+      var letterOrNumber = Math.floor(Math.random() * 2);
+      if (letterOrNumber === 1){
+        return startWordContest(session);
+      } else {
+        return startNumberContest(session);
+      }
     },
 
     treatAnswer: (session) => {
@@ -76,21 +74,31 @@ module.exports = (function() {
       if (!userName) {
         userName = "";
       }
-      let word = session.attributes['word'];
-      let answer = session.event.request.intent.slots.wordAnswer.value;
-
-      session.handler.state = stateContext.states.BETWEEN_QUESTIONS;
+      let word = "" + session.attributes['word'];
+      word = word.trim().toLowerCase();
+      if(word === "start" || word === "again"){
+        return session.emitWithState('StartIntent');
+      }
+      let wordAnswer = session.event.request.intent.slots.wordAnswer.value;
+      let numberAnswer = session.event.request.intent.slots.numberAnswer.value;
+      let answer = wordAnswer;
+      if (!answer){
+        answer = numberAnswer;
+      }
+      answer = "" + answer;
 
       if (!word) {
+        session.handler.state = stateContext.states.BETWEEN_QUESTIONS;
         session.emit(':ask', `${userName} start a contest by sating <break time="0.5s"/>  start`, `say <break time="0.5s"/>  start a game <break time="0.5s"/>  to start a contest.`);
       } else if (!answer) {
         session.emit(':ask', `I couldn\'t catch your answer ${userName} <break time="0.5s"/>  please repeat it`, `please repeat your answer.`);
       } else {
+        session.handler.state = stateContext.states.BETWEEN_QUESTIONS;
         session.attributes['previousWord'] = word;
         session.attributes['previousAnswer'] = answer;
         session.attributes['word'] = undefined;
-        if (word.trim().toLowerCase() === answer.trim().toLowerCase()) {
-          session.emit(':ask', `Congratulations ${userName} <break time="0.5s"/>  you found the word ${word}. say <break time="0.5s"/>  start a game <break time="0.5s"/>  to start another contest.`,
+        if (word === answer.trim().toLowerCase()) {
+          session.emit(':ask', `Congratulations ${userName} <break time="0.5s"/>  you found the answer ${word}. say <break time="0.5s"/>  start a game <break time="0.5s"/>  to start another contest.`,
             `say <break time="0.5s"/>  start a game <break time="0.5s"/>  to start another contest.`);
         } else {
           session.emit(':ask', `Sorry ${userName} <break time="0.5s"/>  you said ${answer} but the answer was <emphasis level="strong">${word}</emphasis>. say <break time="0.5s"/>  start a game <break time="0.5s"/>  to start another contest.`,
@@ -100,7 +108,7 @@ module.exports = (function() {
     },
 
     repeatQuestion: (session) => {
-      let word = session.attributes['word'];
+      let word = "" + session.attributes['word'];
       let userName = session.attributes['userName'];
       if (!userName) {
         userName = "";
@@ -111,7 +119,7 @@ module.exports = (function() {
         let number = word.trim().length;
 
         session.emit(':ask',
-          `we\'re looking for a word of ${number} letters <break time="1s"/>  <emphasis level="strong"><say-as interpret-as="spell-out">${word}</say-as></emphasis>`,
+          `the answer to guess is <break time="1s"/>  <emphasis level="strong"><say-as interpret-as="spell-out">${word}</say-as></emphasis>`,
           `I repeat <break time="0.5s"/>  <emphasis level="strong"><say-as interpret-as="spell-out">${word}</say-as></emphasis>`);
       }
     },
@@ -134,10 +142,53 @@ module.exports = (function() {
     cancel: (session) => {
       session.emit(':tell', 'OK.');
     },
+    unhandledBetweenQuestion: (session) => {
+
+      //Sometimes Alexa mixes up the StartIntent and the AnswerIntent if the user says 1 word to restart a contest
+      if (session.request.intent.name === "AnswerIntent"){
+        let wordAnswer = session.event.request.intent.slots.wordAnswer.value;
+        let numberAnswer = session.event.request.intent.slots.numberAnswer.value;
+        let answer = wordAnswer;
+        if (!answer){
+          answer = numberAnswer;
+        }
+        answer = ("" + answer).trim().toLowerCase();
+        if(answer === "start" || answer === "again"){
+          return session.emitWithState('StartIntent');
+        }
+      }
+
+      session.emitWithState('AMAZON.HelpIntent');
+    },
 
     unhandled: (session) => {
+
       session.emitWithState('AMAZON.HelpIntent');
     }
 
   }
 })();
+
+function startWordContest(session){
+  let word = getAWord();
+  session.attributes['word'] = word;
+  let number = word.trim().length;
+  session.handler.state = stateContext.states.QUESTIONNING;
+
+  session.emit(':ask',
+    `we\'re looking for a word of ${number} letters <break time="1s"/>  <say-as interpret-as="spell-out">${word}</say-as>`,
+    `I repeat <break time="0.5s"/>  <say-as interpret-as="spell-out">${word}</say-as>`);
+}
+
+function startNumberContest(session){
+  const firstNumber = Math.floor(Math.random() * 99);
+  const secondNumber = Math.floor(Math.random() * 99);
+  let word = firstNumber + secondNumber;
+  session.attributes['word'] = word;
+  let number = ("" + word).trim().length;
+  session.handler.state = stateContext.states.QUESTIONNING;
+
+  session.emit(':ask',
+    `we\'re looking for a number of ${number} digits <break time="1s"/>  <say-as interpret-as="spell-out">${firstNumber}</say-as> plus <say-as interpret-as="spell-out">${secondNumber}</say-as>`,
+    `I repeat <break time="0.5s"/>  <say-as interpret-as="spell-out">${word}</say-as>`);
+}
